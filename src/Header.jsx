b213f74672ceeb73a1aeb45e5c8601749aa874cc
beommaker <beom.maker@gmail.com>
2017-11-29 16:46:52 +0900

    feat(header): add-contents
    https://app.asana.com/0/487815437712560/487815437712584

--- src/Header.jsx
+++ src/Header.jsx
@@ -4,24 +4,26 @@ import './Header.css';
 class Header extends Component {
     render() {
         return (
-            <div className='header-top'>
-                <p>Boutique de Masques en tissu Coréen</p>
+            <div className='header'>
+                <div className='header-top'>
+                    <span>Boutique de Masques en tissu Coréen</span>
+                </div>
+                <div className='header-logo'>
+                    <img src="" alt="Mimik_logo" />
+                </div>
+                <div className='header-menu'>
+                    <ul>
+                        <li>PRODUCTS</li>
+                        <li>COMMENT UTILISER?</li>
+                        <li>CONTACTEZ-NOUS</li>
+                        <li>INSCRIPTION</li>
+                        <li>REVIEW</li>
+                        <li><img src="" alt="cart" /></li>
+                        <li><img src="" alt="instagram" /></li>
+                    </ul>
+                </div>
             </div>
-            <div className='header-logo'>
-                <img src="" alt="Mimik_logo"/>
-            </div>
-            <div className='header-list'>
-                <ul>
-                    <li>PRODUCTS</li>
-                    <li>COMMENT UTILISER?</li>
-                    <li>CONTACTEZ-NOUS</li>
-                    <li>INSCRIPTION</li>
-                    <li>REVIEW</li>
-                    <li><img src="" alt="cart"/></li>
-                    <li><img src="" alt="instagram"/></li>
-                </ul>
-            </div>  
-        ) 
+        )
     }
 }
 
